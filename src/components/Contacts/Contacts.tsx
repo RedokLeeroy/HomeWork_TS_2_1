d6f1cdcbf119989e3e21f72d0cd27e533ac96984
RedokLeeroy <Lys1999@ukr.net>
2022-09-21 17:36:16 +0300

    .

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,6 @@
+import PropTypes from "prop-types"
+import s from "./Contacts.module.css"
+
+import { Item } from "./Item"
+export const Contacts = ({contact, onDelete}: {contact: [], }) => {return <ul className={s.list}>{contact.map(({id, name, phone}) => <Item key={id} name={name} phone={phone} onDelete={onDelete} id={id} />)}</ul> }
+
